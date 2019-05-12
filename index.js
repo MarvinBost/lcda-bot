6dbe4601b2c50542da06ebab7dff5f65e04611f5
Marvin Bost <43963200+MarvinBost@users.noreply.github.com>
2019-05-12 21:42:07 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,102 @@
-const Discord=require("discord.js"),client=new Discord.Client;function del(msg,nb){msg.channel.fetchMessages({limit:nb}).then(messages=>{msg.channel.bulkDelete(messages)})}client.on("ready",()=>{client.user.setActivity("Make with 🖤 & ☕ by Max Zander"),console.log(`Logged in as ${client.user.tag}!`)}),client.on("message",msg=>{const purg=msg.guild.channels.find(channels=>"le-purgatoire"===channels.name),gen=msg.guild.channels.find(channels=>"ardoise-public"===channels.name),val=msg.guild.roles.find(roles=>"Clients"===roles.name),noval=msg.guild.roles.find(roles=>"Ames Errantes"===roles.name);if(msg.channel==gen&&("!discord"===msg.content&&(msg.delete(1e3),msg.reply("Vous pouvez invitez vos amis avec ce lien : \n https://discord.gg/KaUQJpU")),"!youtube"===msg.content&&(msg.delete(1e3),msg.reply("La chaine du comptoire : \nhttps://www.youtube.com/channel/UCPFaB1cT8ZiC_K8WmqNJfXA?view_as=public")),"ping"===msg.content&&(msg.reply("Pong!"),msg.delete())),msg.channel==purg&&("LCDA"===msg.content&&(del(msg,2),msg.member.addRole(val),msg.member.removeRole(noval)),"lu et approuvé"===msg.content&&msg.reply("Veux-tu des lunettes ? https://lcda.marvinbost.fr/rules.html"),"lu et accepté"===msg.content&&msg.reply("Tiens le lien tu peux lire les règles ? https://lcda.marvinbost.fr/rules.html"),"accepté"===msg.content&&msg.reply("Est-ce que tu le fais exprès ou tu as juste étais bercée trop prés du mur ? https://lcda.marvinbost.fr/rules.html")),msg.content.startsWith("!prune")){var nb=msg.content.substr(6);if(nb++,!msg.channel.permissionsFor(msg.author).hasPermission("MANAGE_MESSAGES"))return void message.channel.send("Désolé, vous n'avez pas la permission d'exécuter la commande \""+msg.content+'"');if(!msg.channel.permissionsFor(client.user).hasPermission("MANAGE_MESSAGES"))return void msg.channel.send("Désolé, je n'ai pas la permission d'exécuter la commande \""+msg.content+'"');"text"==msg.channel.type&&msg.channel.fetchMessages({limit:nb}).then(messages=>{msg.channel.bulkDelete(messages),messagesDeleted=messages.array().length,msg.channel.send("Suppression des messages réussie. Nombre total de messages supprimés: "+messagesDeleted),del(msg,1)}).catch(err=>{console.log("Error while doing Bulk Delete"),console.log(err)})}if("!purge"===msg.content&&msg.author.hasPermission("KICK_MEMBERS")){let list=msg.guild.roles.find(roles=>"Clients"===roles.name).members;list.map(member=>member.removeRole(val)),list.map(member=>member.addRole(noval)),console.log("Une purge a été faite")}}),client.on("guildMemberAdd",member=>{const channel=member.guild.channels.find(channels=>"le-purgatoire"===channels.name),noval=member.guild.roles.find(roles=>"Ames Errantes"===roles.name);channel&&(member.addRole(noval),channel.send(`Bienvenue ${member} ! pour être sûr que tout le monde lit bien les règles, et qu’ils les respectent vous devrez entrer une phrase qui sera inscrite dans les règles qui nous certifiera que vous les avez lus et accepté.\n Les règles : https://lcda.marvinbost.fr/rules.html`))}),client.on("guildMemberRemove",member=>{member.guild.channels.find(channels=>"ardoise-public"===channels.name).send(`**${member.displayName}** est parti du serveur..... Bye Bye`)}),client.login(process.env.TOKEN);
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+client.on('ready', () => {
+    client.user.setActivity("Make with 🖤 & ☕ by Max Zander");
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+function del(msg, nb) {
+    msg.channel.fetchMessages({
+        limit: nb
+    })
+        .then(messages => {
+            msg.channel.bulkDelete(messages);
+
+        })
+}
+client.on('message', msg => {
+    const purg = msg.guild.channels.find(channels => channels.name === 'le-purgatoire');
+    const gen = msg.guild.channels.find(channels => channels.name === 'ardoise-public');
+    const val = msg.guild.roles.find(roles => roles.name === 'Clients');
+    const noval = msg.guild.roles.find(roles => roles.name === 'Ames Errantes');
+
+    if (msg.channel == gen) {
+        if (msg.content === '!discord') {
+            msg.delete(1000);
+            msg.reply('Vous pouvez invitez vos amis avec ce lien : \n https://discord.gg/KaUQJpU');
+        }
+        if (msg.content === '!youtube') {
+            msg.delete(1000);
+            msg.reply('La chaine du comptoire : \nhttps://www.youtube.com/channel/UCPFaB1cT8ZiC_K8WmqNJfXA?view_as=public');
+        }
+        if (msg.content === 'ping') {
+            msg.reply('Pong!');
+            msg.delete();
+        }
+    }
+    if (msg.channel == purg) {
+        if (msg.content === 'LCDA') {
+            del(msg, 2);
+            msg.member.addRole(val);
+            msg.member.removeRole(noval);
+
+        }
+        if (msg.content === 'lu et approuvé') {
+            msg.reply("Veux-tu des lunettes ? https://lcda.marvinbost.fr/rules.html");
+        }
+        if (msg.content === 'lu et accepté') {
+            msg.reply("Tiens le lien tu peux lire les règles ? https://lcda.marvinbost.fr/rules.html");
+        }
+        if (msg.content === 'accepté') {
+            msg.reply("Est-ce que tu le fais exprès ou tu as juste étais bercée trop prés du mur ? https://lcda.marvinbost.fr/rules.html");
+        }
+    }
+    if (msg.content.startsWith("!prune")) {
+        var nb = msg.content.substr(6);
+        nb++;
+
+        if (!msg.channel.permissionsFor(msg.author).hasPermission("MANAGE_MESSAGES")) {
+            message.channel.send("Désolé, vous n'avez pas la permission d'exécuter la commande \"" + msg.content + "\"");
+            return;
+        } else if (!msg.channel.permissionsFor(client.user).hasPermission("MANAGE_MESSAGES")) {
+            msg.channel.send("Désolé, je n'ai pas la permission d'exécuter la commande \"" + msg.content + "\"");
+            return;
+        }
+        if (msg.channel.type == 'text') {
+            msg.channel.fetchMessages({
+                limit: nb
+            })
+                .then(messages => {
+                    msg.channel.bulkDelete(messages);
+                    messagesDeleted = messages.array().length;
+                    msg.channel.send("Suppression des messages réussie. Nombre total de messages supprimés: " + messagesDeleted);
+                    del(msg, 1);
+                })
+                .catch(err => {
+                    console.log('Error while doing Bulk Delete');
+                    console.log(err);
+                });
+        }
+    }
+    if (msg.content === '!purge' && msg.author.hasPermission('KICK_MEMBERS')) {
+        let list = msg.guild.roles.find(roles => roles.name === 'Clients').members;
+        list.map(member => member.removeRole(val));
+        list.map(member => member.addRole(noval));
+        console.log("Une purge a été faite");
+    }
+});
+
+client.on('guildMemberAdd', member => {
+    const channel = member.guild.channels.find(channels => channels.name === 'le-purgatoire');
+    const noval = member.guild.roles.find(roles => roles.name === 'Ames Errantes');
+    if (!channel) return;
+    member.addRole(noval);
+    channel.send(`Bienvenue ${member} ! pour être sûr que tout le monde lit bien les règles, et qu’ils les respectent vous devrez entrer une phrase qui sera inscrite dans les règles qui nous certifiera que vous les avez lus et accepté.\n Les règles : https://lcda.marvinbost.fr/rules.html`);
+});
+
+client.on('guildMemberRemove', (member) => {
+    member.guild.channels.find(channels => channels.name === 'ardoise-public').send(`**${member.displayName}** est parti du serveur..... Bye Bye`);
+});
+client.login(process.env.TOKEN);
